22de9d95cf2ea4e57c8cb9cbe8bbe03819a60fc4
javreza <jvrza20@gmail.com>
2024-11-20 18:01:50 -0600

    Added computer turn function.

--- logic.js
+++ logic.js
@@ -1,8 +1,28 @@
-function playRound(humanChoice, computerChoice){
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
 
+const choiceBttns = document.querySelectorAll(".choiceBttn");
+let player;
+let computer;
+let result;
+
+choiceBttns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+}));
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random() *3 +1 )
+    switch(randNum){
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 1:
+            computer = "SCISSORS";
+             break;
+    }
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
